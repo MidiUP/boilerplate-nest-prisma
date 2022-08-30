1183191af6525aa46617f37662e27b5e1e53ccf3
Mateus Mendes <mateusmendesm16@outlook.com>
2022-08-30 16:54:06 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+  await app.listen(process.env.PORT || 8000);
+  logger.log(`System running on port ${process.env.PORT || 8000}`);
+}
+bootstrap();
