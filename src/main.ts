cf4af6edbe6f15149b723d9a3cdfc18c023a52ca
Mateus Mendes <mateusmendesm16@outlook.com>
2022-09-01 12:21:06 -0300

    feat: implemented git hooks

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Main')
-  await app.listen(process.env.PORT || 8000)
-  logger.log(`System running on port ${process.env.PORT || 8000}`)
+  const logger = new Logger('Main');
+  await app.listen(process.env.PORT || 8000);
+  logger.log(`System running on port ${process.env.PORT || 8000}`);
 }
 bootstrap();
