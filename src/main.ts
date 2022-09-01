cdb42e991ce7b38828350f735feb6deabde8b984
Mateus Mendes <mateusmendesm16@outlook.com>
2022-09-01 11:48:12 -0300

    test1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Main');
-  await app.listen(process.env.PORT || 8000);
+  await app.listen(process.env.PORT || 8000)
   logger.log(`System running on port ${process.env.PORT || 8000}`);
 }
-bootstrap()
+bootstrap();
