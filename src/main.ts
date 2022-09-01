6117804411ef5a6ce2a05b6fff7c6624b0075289
Mateus Mendes <mateusmendesm16@outlook.com>
2022-09-01 12:08:03 -0300

    feat: implementado commitlint hook

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Main');
+  const logger = new Logger('Main')
   await app.listen(process.env.PORT || 8000)
   logger.log(`System running on port ${process.env.PORT || 8000}`)
 }
